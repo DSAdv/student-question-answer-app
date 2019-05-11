fac117adc56a2cee91fe63652ca8c190c6935059
David Ikkes <d.ikkes2010@gmail.com>
2019-05-11 17:34:34 +0300

    init commit

---
+++ service/users/models.py
@@ -0,0 +1,59 @@
+from flask import g
+from flask_httpauth import HTTPBasicAuth
+
+from service.app import app, db
+
+from werkzeug.security import check_password_hash, generate_password_hash
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    surname = db.Column(db.String(32), index=True)
+    full_group_name = db.Column(db.String(16))
+    telegram_id = db.Column(db.String(64))
+    password_hash = db.Column(db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+        user = User.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
